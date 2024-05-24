375b23b33c76c5151376131cd812e13a4b2b68fd
Mattéo Ricard <ricardm@bde.enseeiht.fr>
2024-05-24 15:42:59 +0000

    feat: init pagging

---
+++ include/n7OS/paging_old.h
@@ -0,0 +1,56 @@
+/**
+ * @file paging.h
+ * @brief Gestion de la pagination dans le noyau
+ */
+#ifndef _PAGING_H
+#define _PAGING_H
+
+#include <inttypes.h>
+
+
+
+
+/**
+ * @brief Description d'une ligne de la table de page
+ * 
+ * Bit :     | 31              12 | 11 10 9 | 8 7  | 6 | 5 | 4 3  | 2 | 1 | 0 |
+ * Contenu : | adresse de la page | Avail   | RSVD | D | A | RSVD | U | W | P | 
+ */
+typedef struct {
+    uint16_t addr_high;
+    uint8_t addr_low;
+    uint8_t flags;
+} page_table_entry_t;
+
+/**
+ * @brief Une entrée dans la table de page peut être manipulée en utilisant
+ *        la structure page_table_entry_t ou directement la valeur
+ */
+typedef union {
+    page_table_entry_t page_entry;
+    uint32_t value;
+} PTE; // PTE = Page Table Entry 
+
+/**
+ * @brief Une table de page (PageTable) est un tableau de descripteurs de page
+ * 
+ */
+typedef PTE * PageTable;
+
+/**
+ * @brief Cette fonction initialise le répertoire de page, alloue les pages de table du noyau
+ *        et active la pagination
+ * 
+ */
+void initialise_paging();
+
+/**
+ * @brief Cette fonction alloue une page de la mémoire physique à une adresse de la mémoire virtuelle
+ * 
+ * @param address       Adresse de la mémoire virtuelle à mapper
+ * @param is_writeable  Si is_writeable == 1, la page est accessible en écriture
+ * @param is_kernel     Si is_kernel == 1, la page ne peut être accédée que par le noyau
+ * @return PageTable    La table de page modifiée
+ */
+PageTable alloc_page_entry(uint32_t address, int is_writeable, int is_kernel);
+#endif
--- kernel/paging.c
+++ kernel/paging.c
@@ -1,24 +1,100 @@
-#include <n7OS/paging.h>
-#include <stddef.h> // nécessaire pour NULL
+#include "n7OS/paging.h"
+#include "n7OS/kheap.h"
+#include "string.h"
+#include "n7OS/mem.h"
+#include "stdio.h"
+
+PageDirectory pageDirectory;
+
+/*
+void handler_page_fault(registers_t reg) {
+       // A page fault has occurred.
+    // The faulting address is stored in the CR2 register.
+    uint32_t faulting_address;
+    __asm__ __volatile__("mov %%cr2, %0" : "=r" (faulting_address));
+    
+    // The error code gives us details of what happened.
+    int present = !(reg.err_code & 0x1); // Page not present
+    int rw = reg.err_code & 0x2;           // Write operation?
+    int us = reg.err_code & 0x4;           // Processor was in user-mode?
+    int reserved = reg.err_code & 0x8;     // Overwritten CPU-reserved bits of page entry?
+    int id = reg.err_code & 0x10;          // Caused by an instruction fetch?
+
+    // Output an error message.
+    printf("Page fault! ( ");
+    if (present) {printf("present ");}
+    if (rw) {printf("read-only ");}
+    if (us) {printf("user-mode ");}
+    if (reserved) {printf("reserved ");}
+    if (id) {printf("instruction ");}
+    printf(") at 0x%x\n", faulting_address);
+    PANIC("Page fault");
+}*/
+
+void setPageEntry(PTE *page_table_entry, uint32_t new_page, int is_writeable, int is_kernel) {
+    page_table_entry->page_entry.present= 1;
+    page_table_entry->page_entry.accessed= 0;
+    page_table_entry->page_entry.dirty= 0;
+    page_table_entry->page_entry.rw= is_writeable;
+    page_table_entry->page_entry.user= ~is_kernel;
+    page_table_entry->page_entry.page= new_page>>12;
+}
 
 void initialise_paging() {
-    // Allouer une zone mémoire pour le répertoire de pages
-    PageTable *pgtab = (PageTable *) kmalloc_a(sizeof(PageTable));
 
-    // Initialiser les tables de pages
+    uint32_t index= 0;
+    
+    init_mem();
 
-    // Charger le répertoire de pages dans le registre CR3
+    pageDirectory= (PageDirectory) kmalloc_a (sizeof(PDE)*1024);
+    memset(pageDirectory, 0, sizeof(PDE)*1024);
+    
+    for (int i= 0; i<1024; ++i) {
+        PageTable new_page_table= (PageTable) kmalloc_a(sizeof(PTE)*1024);
+        memset(new_page_table, 0, sizeof(PTE)*1024);
+        pageDirectory[i].value= (uint32_t) new_page_table|PAGE_PRESENT|PAGE_RW;
+        index= (uint32_t) new_page_table + sizeof(PTE) * 1024;
+    }
 
-    // Lancez la pagination en activant le bit PG du registre CR0
+    for (int i= 0; i<index; i += PAGE_SIZE) {
+        alloc_page_entry(i, 1, 1);
+    }
+
+    //register_interrupt_handler(14, handler_page_fault);
+
+    //loadPageDirectory((unsigned int *)pageDirectory);
+    //enablePaging();
+
 }
 
-PageTable alloc_page_entry(uint32_t address, int is_writeable, int is_kernel) {
-    //PageTable pgtab = 
-//
-    //page_entry_t *entry = 
-    //entry->addr_low = address & 0xF;
-    //entry->addr_high = (address >> 4) & 0xFFFF;
-    //entry->flags = (1 - is_kernel) << 2 | is_writeable << 1 | 1;
-    //entry->zero = 0;
-    //return pgtab;
+PageTable alloc_page_entry(uint32_t address, int is_writeable, int is_kernel ) {
+    // address = adresse virtuelle à allouer 
+    // address = idx_PDE | idx_PTE | offset
+    //             10    |    10   |   12
+
+    int idx_pagedir = address >> 22; // calcul de la position de la table de page dans le répertoire de page
+    PageTable page_table;
+    PDE page_dir_entry = pageDirectory[idx_pagedir]; // on recupere l'entree dans le répertoire de page
+    // une entree contient l'adresse de la table de page + bits de controle
+    
+    //page_table= ...; // on recupere l'adresse de la page de table dans le répertoire de page
+    page_table = (PageTable)(page_dir_entry.value & 0xFFFFF000); // on recupere l'adresse de la page de table dans le répertoire de page
+    
+    //uint32_t phy_page= ...; // recherche d'une page libre dans la memoire physique
+    uint32_t phy_page = findfreePage(); // recherche d'une page libre dans la memoire physique
+    
+    //int idx_pagetab= ...; // calcul de la position de la page dans la table de page
+    int idx_pagetab = (address >> 12) & 0x3FF; // calcul de la position de la page dans la table de page
+
+    
+        
+
+    
+    // mise a jour de l'entree dans la page de table
+    setPageEntry(&page_table[idx_pagetab], phy_page, is_writeable, is_kernel);
+
+    return page_table;
 }
+
+
+
